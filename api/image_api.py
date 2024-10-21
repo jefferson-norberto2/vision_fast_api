978f06d22f4d1eae05fdd7c2d2d7406c4db84b45
jefferson-norberto2 <jmn@cin.ufpe.br>
2024-10-21 17:13:04 -0300

    ERMOVE SENSORS

--- api/image_api.py
+++ api/image_api.py
@@ -15,23 +15,11 @@ class ImageAPI(FastAPI):
 
         self.add_api_route('/', self.home, methods=["GET"])
         self.add_api_websocket_route("/ws/camera", self.websocket_endpoint)
-        self.add_api_websocket_route("/sensors", self.show_sensors)
         
-
     async def home(self):
         html_file_path = Path("pages/home.html")
         html_content = html_file_path.read_text(encoding="utf-8")
         return HTMLResponse(html_content)
-    
-    async def show_sensors(self, *,
-        websocket: WebSocket):
-        await self.manager.connect(websocket)
-        try:
-            while True:
-                data = await websocket.receive_text()
-                print(f"Received data: {data}")  # Exibe os dados recebidos no console
-        except WebSocketDisconnect:
-            self.manager.disconnect(websocket)
 
     async def websocket_endpoint(
         self,
